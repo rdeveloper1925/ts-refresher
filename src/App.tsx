3b637f5df6bb52722d435398571b3c3d273df4f4
Rodney Mathias <Rodney@ourkidsmedia.com>
2025-09-25 21:35:34 -0400

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import BaseLayout from './layouts/BaseLayout'
+import BlogHeader from './components/mine/BlogHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BaseLayout>
-    <div className='border-2 rounded-2xl'>
-      <div className='flex justify-around align-baseline'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button className='bg-neutral-300 fill-orange-800' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-    </BaseLayout>
+    <>
+      <BlogHeader/>
+    </>
     
   )
 }
